e4aabbfcfd1266552fe7355c57e42dda889a056d
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-10-16 13:16:09 +0700

    fix variabel candi

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key, required candi});
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +19,7 @@ class DetailScreen extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20),
                   child: Image.asset(
-                    'candi',
+                    candi.imageAsset,
                     width: double.infinity,
                     height: 300,
                   ),
